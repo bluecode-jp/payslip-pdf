351cae8f94a52dfb4f6f91e0d43ae49233db6d86
obviouswhy <sergilockerz@gmail.com>
2021-11-02 12:02:34 +0900

    simple navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import EstimationForm from './screens/EstimationForm'
+import ResultPage from './screens/ResultPage'
 
 const App = () => {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path={'/'} exact component={() => <EstimationForm />} />
+          <Route path={'/result'} exact component={ResultPage} />
+          <Route path={'*'} exact component={() => <div> NOT FOUND!</div>} />
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
---
+++ src/screens/EstimationForm/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const EstimationForm = () => {
+  return <div>Estimation Form</div>
+}
+
+export default EstimationForm
