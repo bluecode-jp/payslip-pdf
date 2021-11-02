f4fe6b48bc50585d615430572e1f601353d6aed9
obviouswhy <sergilockerz@gmail.com>
2021-11-02 13:55:32 +0900

    implemented dynamic form

--- src/screens/EstimationForm/index.js
+++ src/screens/EstimationForm/index.js
@@ -1,7 +1,89 @@
-import React from 'react'
+import { useState } from 'react'
 
 const EstimationForm = () => {
-  return <div>Estimation Form</div>
+  const [title, setTitle] = useState('')
+  const [formFields, setFormFields] = useState([
+    { index: 0, name: '', quantity: 0, price: 0 },
+  ])
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <h2 style={{ alignSelf: 'center' }}>Estimation Form</h2>
+      <label>Title</label>
+      <input
+        type="text"
+        placeholder="Fruit Gift"
+        onChange={e => setTitle(e.target.value)}
+      />
+      <table>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>Quantity</td>
+            <td>Price</td>
+            <td></td>
+          </tr>
+          {formFields.map(field => (
+            <tr key={field.index}>
+              <td>
+                <input
+                  type={'text'}
+                  placeholder={'Name'}
+                  defaultValue={field.name}
+                  onChange={e => (field.name = e.target.value)}
+                />
+              </td>
+              <td>
+                <input
+                  type={'number'}
+                  defaultValue={field.quantity}
+                  onChange={e => (field.quantity = e.target.value)}
+                />
+              </td>
+              <td>
+                <input
+                  type={'number'}
+                  defaultValue={field.price}
+                  onChange={e => (field.price = e.target.value)}
+                />
+              </td>
+              <td>
+                <button
+                  onClick={() =>
+                    formFields.length > 1 &&
+                    setFormFields(
+                      formFields.filter(item => item.index != field.index),
+                    )
+                  }>
+                  Remove
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button
+        onClick={() =>
+          setFormFields(prevState => [
+            ...prevState,
+            {
+              index: prevState.length,
+              name: '',
+              quantity: 0,
+              price: 0,
+            },
+          ])
+        }>
+        Add Field
+      </button>
+      <button
+        onClick={() => {
+          console.log({ title, fields: formFields })
+        }}>
+        Submit
+      </button>
+    </div>
+  )
 }
 
 export default EstimationForm
