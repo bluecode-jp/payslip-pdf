2eea038396891da6ffd53c64d700120aa702a169
obviouswhy <sergilockerz@gmail.com>
2021-11-05 15:36:40 +0900

    added placeholder to textarea

--- src/screens/PayslipForm/forms/NoteForm/index.js
+++ src/screens/PayslipForm/forms/NoteForm/index.js
@@ -10,13 +10,19 @@ const NoteForm = () => {
               style={{
                 textAlign: 'center',
                 backgroundColor: '#d2eafd',
+                border: '0.5px #00000088 solid',
               }}>
               お知らせ
             </td>
           </tr>
           <tr>
             <td>
-              <textarea rows={6} />
+              <textarea
+                placeholder={
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a felis malesuada, ultrices odio vitae, porta justo. '
+                }
+                rows={6}
+              />
             </td>
           </tr>
         </tbody>
