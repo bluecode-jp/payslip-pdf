4e4014d61abb8787b6cad30857d87622f79dca4c
obviouswhy <sergilockerz@gmail.com>
2021-11-08 16:15:17 +0900

    removed unused buttons of PDFViewer

--- src/screens/PDFViewer/index.js
+++ src/screens/PDFViewer/index.js
@@ -1,10 +1,12 @@
 import { useEffect } from 'react'
+import { printPlugin } from '@react-pdf-viewer/print'
 import { Viewer, Worker } from '@react-pdf-viewer/core'
 import { useHistory, useLocation } from 'react-router-dom'
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
+import { getFilePlugin } from '@react-pdf-viewer/get-file'
 
 import '@react-pdf-viewer/core/lib/styles/index.css'
 import '@react-pdf-viewer/default-layout/lib/styles/index.css'
+import '@react-pdf-viewer/print/lib/styles/index.css'
 
 const PDFViewer = () => {
   const location = useLocation()
@@ -14,7 +16,11 @@ const PDFViewer = () => {
     !location.state && history.replace('/')
   }, [])
 
-  const defaultLayoutPluginInstance = defaultLayoutPlugin()
+  const printPluginInstance = printPlugin()
+  const getFilePluginInstance = getFilePlugin()
+  const { PrintButton } = printPluginInstance
+  const { DownloadButton } = getFilePluginInstance
+
   return (
     <>
       {location.state && (
@@ -22,13 +28,34 @@ const PDFViewer = () => {
           style={{
             width: '100vw',
             height: '100vh',
+            border: '1px solid rgba(0, 0, 0, 0.3)',
+            display: 'flex',
+            flexDirection: 'column',
           }}>
-          <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
-            <Viewer
-              fileUrl={location.state.pdfFile}
-              plugins={[defaultLayoutPluginInstance]}
-            />
-          </Worker>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+              backgroundColor: '#eeeeee',
+              borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+              padding: '4px',
+            }}>
+            <PrintButton />
+            <DownloadButton />
+          </div>
+          <div
+            style={{
+              flex: 1,
+              overflow: 'hidden',
+            }}>
+            <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
+              <Viewer
+                fileUrl={location.state.pdfFile}
+                plugins={[printPluginInstance, getFilePluginInstance]}
+              />
+            </Worker>
+          </div>
         </div>
       )}
     </>
