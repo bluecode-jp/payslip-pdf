5ef13b0c5dd0b046fafdefa53fc1a7c8d9bf11b6
obviouswhy <sergilockerz@gmail.com>
2022-02-21 14:28:39 +0900

    link form state with pdf for form5 and form6

--- src/screens/WithholdForm/FormsGrid/Forms/Form5/index.js
+++ src/screens/WithholdForm/FormsGrid/Forms/Form5/index.js
@@ -1,16 +1,37 @@
 import styles from './styles.module.css'
+import PropTypes from 'prop-types'
 
-function Form5() {
+function Form5({ formValues, setFormValues }) {
   return (
     <div className={styles.form5Wrapper}>
       <div className={`${styles.column} ${styles.firstCol}`}>
         <label>社会保険料等の金額</label>
         <div className={styles.numberContainer}>
           <div className={`${styles.sen} ${styles.uchi}`}>
-            <input type={'text'} />
+            <input
+              type={'text'}
+              value={formValues.form5.socialInsurance.sen}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form5.socialInsurance.sen = e.target.value
+                  return formValues
+                })
+              }
+            />
           </div>
           <div className={styles.yen}>
-            <input type={'text'} />
+            <input
+              type={'text'}
+              value={formValues.form5.socialInsurance.yen}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form5.socialInsurance.yen = e.target.value
+                  return formValues
+                })
+              }
+            />
           </div>
         </div>
       </div>
@@ -18,10 +39,30 @@ function Form5() {
         <label>生命保険料の控除額</label>
         <div className={styles.numberContainer}>
           <div className={styles.sen}>
-            <input type={'text'} />
+            <input
+              type={'text'}
+              value={formValues.form5.lifeInsurance.sen}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form5.lifeInsurance.sen = e.target.value
+                  return formValues
+                })
+              }
+            />
           </div>
           <div className={styles.yen}>
-            <input type={'text'} />
+            <input
+              type={'text'}
+              value={formValues.form5.lifeInsurance.yen}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form5.lifeInsurance.yen = e.target.value
+                  return formValues
+                })
+              }
+            />
           </div>
         </div>
       </div>
@@ -29,10 +70,30 @@ function Form5() {
         <label>地震保険料の控除額</label>
         <div className={styles.numberContainer}>
           <div className={styles.sen}>
-            <input type={'text'} />
+            <input
+              type={'text'}
+              value={formValues.form5.earthquakeInsurance.sen}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form5.earthquakeInsurance.sen = e.target.value
+                  return formValues
+                })
+              }
+            />
           </div>
           <div className={styles.yen}>
-            <input type={'text'} />
+            <input
+              type={'text'}
+              value={formValues.form5.earthquakeInsurance.yen}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form5.earthquakeInsurance.yen = e.target.value
+                  return formValues
+                })
+              }
+            />
           </div>
         </div>
       </div>
@@ -40,15 +101,40 @@ function Form5() {
         <label>住宅借入金等特別控除の額</label>
         <div className={styles.numberContainer}>
           <div className={styles.sen}>
-            <input type={'text'} />
+            <input
+              type={'text'}
+              value={formValues.form5.specialDeduction.sen}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form5.specialDeduction.sen = e.target.value
+                  return formValues
+                })
+              }
+            />
           </div>
           <div className={styles.yen}>
-            <input type={'text'} />
+            <input
+              type={'text'}
+              value={formValues.form5.specialDeduction.yen}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form5.specialDeduction.yen = e.target.value
+                  return formValues
+                })
+              }
+            />
           </div>
         </div>
       </div>
     </div>
   )
+}
+
+Form5.propTypes = {
+  formValues: PropTypes.object,
+  setFormValues: PropTypes.func,
 }
 
 export default Form5
--- src/screens/WithholdForm/FormsGrid/Forms/Form6/index.js
+++ src/screens/WithholdForm/FormsGrid/Forms/Form6/index.js
@@ -1,12 +1,28 @@
 import styles from './styles.module.css'
+import PropTypes from 'prop-types'
 
-function Form6() {
+function Form6({ formValues, setFormValues }) {
   return (
     <div className={styles.form6Wrapper}>
       <label>(摘要)</label>
-      <input type={'text'} />
+      <input
+        type={'text'}
+        value={formValues.form6.summary}
+        onChange={e =>
+          setFormValues(prevState => {
+            const formValues = { ...prevState }
+            formValues.form6.summary = e.target.value
+            return formValues
+          })
+        }
+      />
     </div>
   )
+}
+
+Form6.propTypes = {
+  formValues: PropTypes.object,
+  setFormValues: PropTypes.func,
 }
 
 export default Form6
