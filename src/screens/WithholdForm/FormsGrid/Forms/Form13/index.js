07735a768dedebf78680a9c4b5292d6336ea364a
obviouswhy <sergilockerz@gmail.com>
2022-02-21 15:24:30 +0900

    link form state with pdf for form13

--- src/screens/WithholdForm/FormsGrid/Forms/Form13/index.js
+++ src/screens/WithholdForm/FormsGrid/Forms/Form13/index.js
@@ -1,6 +1,32 @@
+import PropTypes from 'prop-types'
 import styles from './styles.module.css'
 
-function Form13() {
+// form13: {
+//   minor: 1,
+//   foreigner: 2,
+//   deathRetirement: 3,
+//   disasterVictim: 4,
+//   otsuran: 5,
+//   disability: { special: 6, other: 7 },
+//   widow: 8,
+//   singleParent: 9,
+//   workingStudent: 10,
+//   midOrRetirement: {
+//     searching: 11,
+//     retired: 12,
+//     year: 13,
+//     month: 14,
+//     day: 15,
+//   },
+//   beneficiary: {
+//     number: 16,
+//     year: 17,
+//     month: 18,
+//     day: 19,
+//   },
+// },
+
+function Form13({ formValues, setFormValues }) {
   return (
     <div className={styles.form13Wrapper}>
       <div className={styles.firstHalf}>
@@ -14,7 +40,16 @@ function Form13() {
             </label>
           </div>
           <div className={styles.selectContainer}>
-            <select name={'kubun'}>
+            <select
+              name={'minor'}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form13.minor =
+                    e.target.options[e.target.selectedIndex].value
+                  return formValues
+                })
+              }>
               <option value={''}></option>
               <option value={'A'}>A</option>
               <option value={'B'}>B</option>
@@ -32,7 +67,16 @@ function Form13() {
             </label>
           </div>
           <div className={styles.selectContainer}>
-            <select name={'kubun'}>
+            <select
+              name={'foreigner'}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form13.foreigner =
+                    e.target.options[e.target.selectedIndex].value
+                  return formValues
+                })
+              }>
               <option value={''}></option>
               <option value={'A'}>A</option>
               <option value={'B'}>B</option>
@@ -50,7 +94,16 @@ function Form13() {
             </label>
           </div>
           <div className={styles.selectContainer}>
-            <select name={'kubun'}>
+            <select
+              name={'deathRetirement'}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form13.deathRetirement =
+                    e.target.options[e.target.selectedIndex].value
+                  return formValues
+                })
+              }>
               <option value={''}></option>
               <option value={'A'}>A</option>
               <option value={'B'}>B</option>
@@ -68,7 +121,16 @@ function Form13() {
             </label>
           </div>
           <div className={styles.selectContainer}>
-            <select name={'kubun'}>
+            <select
+              name={'disasterVictim'}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form13.disasterVictim =
+                    e.target.options[e.target.selectedIndex].value
+                  return formValues
+                })
+              }>
               <option value={''}></option>
               <option value={'A'}>A</option>
               <option value={'B'}>B</option>
@@ -86,7 +148,16 @@ function Form13() {
             </label>
           </div>
           <div className={styles.selectContainer}>
-            <select name={'kubun'}>
+            <select
+              name={'otsuran'}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form13.otsuran =
+                    e.target.options[e.target.selectedIndex].value
+                  return formValues
+                })
+              }>
               <option value={''}></option>
               <option value={'A'}>A</option>
               <option value={'B'}>B</option>
@@ -108,7 +179,16 @@ function Form13() {
                 </label>
               </div>
               <div className={styles.selectContainer}>
-                <select name={'kubun'}>
+                <select
+                  name={'disabilitySpecial'}
+                  onChange={e =>
+                    setFormValues(prevState => {
+                      const formValues = { ...prevState }
+                      formValues.form13.disability.special =
+                        e.target.options[e.target.selectedIndex].value
+                      return formValues
+                    })
+                  }>
                   <option value={''}></option>
                   <option value={'A'}>A</option>
                   <option value={'B'}>B</option>
@@ -127,7 +207,16 @@ function Form13() {
                 </label>
               </div>
               <div className={styles.selectContainer}>
-                <select name={'kubun'}>
+                <select
+                  name={'disabilityOther'}
+                  onChange={e =>
+                    setFormValues(prevState => {
+                      const formValues = { ...prevState }
+                      formValues.form13.disability.other =
+                        e.target.options[e.target.selectedIndex].value
+                      return formValues
+                    })
+                  }>
                   <option value={''}></option>
                   <option value={'A'}>A</option>
                   <option value={'B'}>B</option>
@@ -148,7 +237,16 @@ function Form13() {
             </label>
           </div>
           <div className={styles.selectContainer}>
-            <select name={'kubun'}>
+            <select
+              name={'widow'}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form13.widow =
+                    e.target.options[e.target.selectedIndex].value
+                  return formValues
+                })
+              }>
               <option value={''}></option>
               <option value={'A'}>A</option>
               <option value={'B'}>B</option>
@@ -166,7 +264,16 @@ function Form13() {
             </label>
           </div>
           <div className={styles.selectContainer}>
-            <select name={'kubun'}>
+            <select
+              name={'singleParent'}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form13.singleParent =
+                    e.target.options[e.target.selectedIndex].value
+                  return formValues
+                })
+              }>
               <option value={''}></option>
               <option value={'A'}>A</option>
               <option value={'B'}>B</option>
@@ -184,7 +291,16 @@ function Form13() {
             </label>
           </div>
           <div className={styles.selectContainer}>
-            <select name={'kubun'}>
+            <select
+              name={'workingStudent'}
+              onChange={e =>
+                setFormValues(prevState => {
+                  const formValues = { ...prevState }
+                  formValues.form13.workingStudent =
+                    e.target.options[e.target.selectedIndex].value
+                  return formValues
+                })
+              }>
               <option value={''}></option>
               <option value={'A'}>A</option>
               <option value={'B'}>B</option>
@@ -210,7 +326,16 @@ function Form13() {
                 </label>
               </div>
               <div className={styles.selectContainer}>
-                <select name={'kubun'}>
+                <select
+                  name={'midOrRetirementSearching'}
+                  onChange={e =>
+                    setFormValues(prevState => {
+                      const formValues = { ...prevState }
+                      formValues.form13.midOrRetirement.searching =
+                        e.target.options[e.target.selectedIndex].value
+                      return formValues
+                    })
+                  }>
                   <option value={''}></option>
                   <option value={'A'}>A</option>
                   <option value={'B'}>B</option>
@@ -228,7 +353,16 @@ function Form13() {
                 </label>
               </div>
               <div className={styles.selectContainer}>
-                <select name={'kubun'}>
+                <select
+                  name={'midOrRetirementRetired'}
+                  onChange={e =>
+                    setFormValues(prevState => {
+                      const formValues = { ...prevState }
+                      formValues.form13.midOrRetirement.retired =
+                        e.target.options[e.target.selectedIndex].value
+                      return formValues
+                    })
+                  }>
                   <option value={''}></option>
                   <option value={'A'}>A</option>
                   <option value={'B'}>B</option>
@@ -246,7 +380,17 @@ function Form13() {
                 </label>
               </div>
               <div className={styles.inputContainer}>
-                <input type={'text'} />
+                <input
+                  type={'text'}
+                  value={formValues.form13.midOrRetirement.year}
+                  onChange={e =>
+                    setFormValues(prevState => {
+                      const formValues = { ...prevState }
+                      formValues.form13.midOrRetirement.year = e.target.value
+                      return formValues
+                    })
+                  }
+                />
               </div>
             </div>
             <div className={styles.columnWrapper}>
@@ -259,7 +403,17 @@ function Form13() {
                 </label>
               </div>
               <div className={styles.inputContainer}>
-                <input type={'text'} />
+                <input
+                  type={'text'}
+                  value={formValues.form13.midOrRetirement.month}
+                  onChange={e =>
+                    setFormValues(prevState => {
+                      const formValues = { ...prevState }
+                      formValues.form13.midOrRetirement.month = e.target.value
+                      return formValues
+                    })
+                  }
+                />
               </div>
             </div>
             <div className={styles.columnWrapper}>
@@ -272,7 +426,17 @@ function Form13() {
                 </label>
               </div>
               <div className={styles.inputContainer}>
-                <input type={'text'} />
+                <input
+                  type={'text'}
+                  value={formValues.form13.midOrRetirement.day}
+                  onChange={e =>
+                    setFormValues(prevState => {
+                      const formValues = { ...prevState }
+                      formValues.form13.midOrRetirement.day = e.target.value
+                      return formValues
+                    })
+                  }
+                />
               </div>
             </div>
           </div>
@@ -290,7 +454,16 @@ function Form13() {
                 </label>
               </div>
               <div className={styles.selectContainer}>
-                <select name={'kubun'}>
+                <select
+                  name={'beneficiary'}
+                  onChange={e =>
+                    setFormValues(prevState => {
+                      const formValues = { ...prevState }
+                      formValues.form13.beneficiary.number =
+                        e.target.options[e.target.selectedIndex].value
+                      return formValues
+                    })
+                  }>
                   <option value={''}></option>
                   <option value={'A'}>A</option>
                   <option value={'B'}>B</option>
@@ -308,7 +481,17 @@ function Form13() {
                 </label>
               </div>
               <div className={styles.inputContainer}>
-                <input type={'text'} />
+                <input
+                  type={'text'}
+                  value={formValues.form13.beneficiary.year}
+                  onChange={e =>
+                    setFormValues(prevState => {
+                      const formValues = { ...prevState }
+                      formValues.form13.beneficiary.year = e.target.value
+                      return formValues
+                    })
+                  }
+                />
               </div>
             </div>
             <div className={styles.columnWrapper}>
@@ -321,7 +504,17 @@ function Form13() {
                 </label>
               </div>
               <div className={styles.inputContainer}>
-                <input type={'text'} />
+                <input
+                  type={'text'}
+                  value={formValues.form13.beneficiary.month}
+                  onChange={e =>
+                    setFormValues(prevState => {
+                      const formValues = { ...prevState }
+                      formValues.form13.beneficiary.month = e.target.value
+                      return formValues
+                    })
+                  }
+                />
               </div>
             </div>
             <div className={styles.columnWrapper}>
@@ -334,7 +527,17 @@ function Form13() {
                 </label>
               </div>
               <div className={styles.inputContainer}>
-                <input type={'text'} />
+                <input
+                  type={'text'}
+                  value={formValues.form13.beneficiary.day}
+                  onChange={e =>
+                    setFormValues(prevState => {
+                      const formValues = { ...prevState }
+                      formValues.form13.beneficiary.day = e.target.value
+                      return formValues
+                    })
+                  }
+                />
               </div>
             </div>
           </div>
@@ -342,6 +545,11 @@ function Form13() {
       </div>
     </div>
   )
+}
+
+Form13.propTypes = {
+  formValues: PropTypes.object,
+  setFormValues: PropTypes.func,
 }
 
 export default Form13
