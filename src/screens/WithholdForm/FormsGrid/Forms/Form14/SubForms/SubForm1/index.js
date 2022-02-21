f95d3e606a3d4edbea08f5a5cff9b1d59cef178d
obviouswhy <sergilockerz@gmail.com>
2022-02-21 15:27:42 +0900

    link form state with pdf for form14

--- src/screens/WithholdForm/FormsGrid/Forms/Form14/SubForms/SubForm1/index.js
+++ src/screens/WithholdForm/FormsGrid/Forms/Form14/SubForms/SubForm1/index.js
@@ -1,6 +1,7 @@
+import PropTypes from 'prop-types'
 import styles from './styles.module.css'
 
-function SubForm1() {
+function SubForm1({ formValues, setFormValues }) {
   return (
     <div className={styles.subform1Wrapper}>
       <div className={`${styles.row} ${styles.firstRow}`}></div>
@@ -9,23 +10,58 @@ function SubForm1() {
           <div className={styles.firstSubrow}>
             <label>住所(居所) 又は所在地</label>
             <div>
-              <input type={'text'} />
+              <input
+                type={'text'}
+                value={formValues.form14.jyusho}
+                onChange={e =>
+                  setFormValues(prevState => {
+                    const formValues = { ...prevState }
+                    formValues.form14.jyusho = e.target.value
+                    return formValues
+                  })
+                }
+              />
             </div>
           </div>
           <div className={styles.secondSubrow}>
             <label>氏名又は名称</label>
             <div>
-              <input type={'text'} />
+              <input
+                type={'text'}
+                value={formValues.form14.name}
+                onChange={e =>
+                  setFormValues(prevState => {
+                    const formValues = { ...prevState }
+                    formValues.form14.name = e.target.value
+                    return formValues
+                  })
+                }
+              />
             </div>
             <label className={styles.phone}>(電話)</label>
             <div>
-              <input type={'text'} />
+              <input
+                type={'text'}
+                value={formValues.form14.phone}
+                onChange={e =>
+                  setFormValues(prevState => {
+                    const formValues = { ...prevState }
+                    formValues.form14.phone = e.target.value
+                    return formValues
+                  })
+                }
+              />
             </div>
           </div>
         </div>
       </div>
     </div>
   )
+}
+
+SubForm1.propTypes = {
+  formValues: PropTypes.object,
+  setFormValues: PropTypes.func,
 }
 
 export default SubForm1
--- src/screens/WithholdForm/FormsGrid/Forms/Form14/index.js
+++ src/screens/WithholdForm/FormsGrid/Forms/Form14/index.js
@@ -1,17 +1,23 @@
+import PropTypes from 'prop-types'
 import styles from './styles.module.css'
 import SubForm1 from './SubForms/SubForm1'
 
-function Form14() {
+function Form14({ formValues, setFormValues }) {
   return (
     <div className={styles.form14Wrapper}>
       <div className={styles.titleContainer}>
         <label style={{ width: '1rem' }}>支 払 者</label>
       </div>
       <div className={styles.subformWrapper}>
-        <SubForm1 />
+        <SubForm1 {...{ formValues, setFormValues }} />
       </div>
     </div>
   )
+}
+
+Form14.propTypes = {
+  formValues: PropTypes.object,
+  setFormValues: PropTypes.func,
 }
 
 export default Form14
